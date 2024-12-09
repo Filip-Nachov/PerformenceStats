7dc2a0e0b0735b865709a6a7ba9b447710746c95
Filip Nachov <fnachov@gmail.com>
2024-12-09 12:14:19 +0100

    Gave up on it and got back the working ver will try to fix it later

--- ServerPerformance.sh
+++ ServerPerformance.sh
@@ -26,6 +26,39 @@ Bar() {
     CMD3=$(DISK)
     CMD4=$(Top5CPU)
 
+     CPU_WIDTH=18
+    MEM_WIDTH=30
+    DISK_WIDTH=40
+    PROC_WIDTH=50
+
+    printf "%-${CPU_WIDTH}s %-${MEM_WIDTH}s %-${DISK_WIDTH}s %-${PROC_WIDTH}s\n" "CPU USAGE" "MEMORY USAGE" "DISK USAGE" "TOP 5 PROCESSES"
+    printf "%-${CPU_WIDTH}s %-${MEM_WIDTH}s %-${DISK_WIDTH}s %-${PROC_WIDTH}s\n" "-----------" "-------------" "------------------------------" "--------------------------------------------------"
+
+    printf "%-${CPU_WIDTH}s" "$(echo "$CMD1" | cut -c1-${CPU_WIDTH})"
+    printf "%-${MEM_WIDTH}s" " "
+    printf "%-${DISK_WIDTH}s" " "
+    printf "%-${PROC_WIDTH}s\n" " "
+
+    while IFS= read -r line; do
+        printf "%-${CPU_WIDTH}s" " "
+        printf "%-${MEM_WIDTH}s" "$(echo "$line" | cut -c1-${MEM_WIDTH})"
+        printf "%-${DISK_WIDTH}s" " "
+        printf "%-${PROC_WIDTH}s\n" " "
+    done <<< "$CMD2"
+
+    while IFS= read -r line; do
+        printf "%-${CPU_WIDTH}s" " "
+        printf "%-${MEM_WIDTH}s" " "
+        printf "%-${DISK_WIDTH}s" "$(echo "$line" | cut -c1-${DISK_WIDTH})"
+        printf "%-${PROC_WIDTH}s\n" " "
+    done <<< "$CMD3"
+
+    while IFS= read -r line; do
+        printf "%-${CPU_WIDTH}s" " "
+        printf "%-${MEM_WIDTH}s" " "
+        printf "%-${DISK_WIDTH}s" " "
+        printf "%-${PROC_WIDTH}s\n" "$(echo "$line" | cut -c1-${PROC_WIDTH})"
+    done <<< "$CMD4"
 }
 
 Bar
