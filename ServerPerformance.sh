8886c85cbb4839583e53228e6d45121dcb206ad7
Filip Nachov <fnachov@gmail.com>
2024-12-09 02:24:12 +0100

    added a bar command that will make the format for this

--- ServerPerformance.sh
+++ ServerPerformance.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+Bar() {
+    echo '     CPU USAGE                   MEM USAGE                        DISK MEMORY                            TOP 5 procceses
+          _____________________      _______________________    _________________________________________     _________________________'
+}
 
 CPU() {
     top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
