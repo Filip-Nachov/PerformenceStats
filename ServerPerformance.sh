6177d54ebc3c1c0183e98896718a3b05c8d53c71
Filip Nachov <fnachov@gmail.com>
2024-12-09 02:10:15 +0100

    added a variable that gets the USED MEM

--- ServerPerformance.sh
+++ ServerPerformance.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
 CPU() {
     top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
 }
+
+MEMORY() {
+    USED_MEM = $(free -h | grep "Mem" | awk '{print $3}')
+}
+
+MEMORY
