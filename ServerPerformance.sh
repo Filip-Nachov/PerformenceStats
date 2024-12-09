d18ed3fa352cc8852644e648a4753b54220367aa
Filip Nachov <fnachov@gmail.com>
2024-12-09 12:11:21 +0100

    removed the width command and the Calculating

--- ServerPerformance.sh
+++ ServerPerformance.sh
@@ -20,27 +20,12 @@ Top5CPU() {
     ps -eo user,pid,%cpu,%mem,etime,comm --sort=-%cpu | head -n 6
 }
 
-CalculateMaxWidth() {
-    local maxWidth=0
-    for str in "$@"; do
-        local length=${#str}
-        if ((length > max_width)); then
-            max_width=$length
-        fi
-    done
-    echo $max_width
-}
-
 Bar() {
     CMD1=$(CPU)
     CMD2=$(MEMORY)
     CMD3=$(DISK)
     CMD4=$(Top5CPU)
 
-    CPU_WIDTH=$(CalculateMaxWidth "CPU USAGE" "$CMD1")
-    MEM_WIDTH=$(CalculateMaxWidth "MEMORY USAGE" "$CMD2")
-    DISK_WIDTH=$(CalculateMaxWidth "DISK USAGE" "$CMD3")
-    PROC_WIDTH=$(CalculateMaxWidth "TOP 5 PROCESSES" "$CMD4")
 }
 
 Bar
